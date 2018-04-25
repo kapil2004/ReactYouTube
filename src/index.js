084f14e5a9bad161a86bf08e29dcfa355d52f173
Kapil Gupta <kapil2004@gmail.com>
2018-04-25 16:16:06 -0400

    show search_bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCkiEYvBrCb_UyiNz0lKa6v7WGc8jiunrw';
 
-// create a new component
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
-// put component's html into dom
 ReactDOM.render(<App />, document.querySelector('.container'));
