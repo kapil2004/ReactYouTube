ce40c40fe90a937c1ec7a334b3fbf6b2e0bf5f38
Kapil Gupta <kapil2004@gmail.com>
2018-04-26 11:30:45 -0400

    add ability to search with search term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,10 +10,15 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
-          onChange={ event => this.setState({ term: event.target.value }) }
+          onChange={ event => this.onInputChange(event.target.value) }
           value={ this.state.term } />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
